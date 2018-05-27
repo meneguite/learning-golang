e92baf65089af68b497f3bc618897ed670bb342e
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2018-05-27 18:24:54 -0300

    Adds an example for provider static files

---
+++ static-files/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+// Example
+// curl -s http://localhost:8080/static/css/styles.css
